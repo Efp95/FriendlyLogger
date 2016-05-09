df1bdcd56198447eda4e8a29361c9dc98b344efa
Efp95 <edsonflorespalma@gmail.com>
2016-05-09 16:04:54 -0500

    Demo Loggers

--- LoggerSolution/FriendlyLogger/Logger/ConsoleLogger.cs
+++ LoggerSolution/FriendlyLogger/Logger/ConsoleLogger.cs
@@ -1,4 +1,5 @@
-﻿using FriendlyLogger.Core;
+﻿using FriendlyLogger.Common;
+using FriendlyLogger.Core;
 using System;
 using System.Collections.Generic;
 
@@ -13,7 +14,60 @@ namespace FriendlyLogger.Logger
 
         public override void Log(Type declaringType, Level level, object message, Exception exception)
         {
-            //
+            var colorSettings = new ConsoleColorManager(level);
+
+            Console.ForegroundColor = colorSettings.Foreground;
+            Console.BackgroundColor = colorSettings.Background;
+
+            if (exception == null)
+                Console.WriteLine(message);
+            else
+                Console.WriteLine("Error: {0}", exception.HResult, message);
+
+            Console.BackgroundColor = ConsoleColorManager.GetDefaultForeground();
         }
+    }
+
+    public class ConsoleColorManager
+    {
+        private ConsoleColor _background;
+        private ConsoleColor _foreground;
+
+
+        public ConsoleColorManager(Level level)
+        {
+            _background = ConsoleColor.Black;
+            _foreground = ManageForeGround(level);
+        }
+
+        public ConsoleColor Foreground
+        {
+            get { return _foreground; }
+            set { _foreground = value; }
+        }
+        public ConsoleColor Background
+        {
+            get { return _background; }
+            set { _background = value; }
+        }
+
+        private ConsoleColor ManageForeGround(Level level)
+        {
+            switch (level.Name)
+            {
+                case Parameters.LevelName.INFO: return ConsoleColor.White;
+                case Parameters.LevelName.DEBUG: return ConsoleColor.Blue;
+                case Parameters.LevelName.WARN: return ConsoleColor.Yellow;
+                case Parameters.LevelName.ERROR: return ConsoleColor.DarkRed;
+                case Parameters.LevelName.FATAL: return ConsoleColor.Red;
+                default: return default(ConsoleColor);
+            }
+        }
+
+        public static ConsoleColor GetDefaultForeground()
+        {
+            return ConsoleColor.White;
+        }
+
     }
 }
--- LoggerSolution/FriendlyLogger/Logger/FileLogger.cs
+++ LoggerSolution/FriendlyLogger/Logger/FileLogger.cs
@@ -1,6 +1,7 @@
 ﻿using FriendlyLogger.Core;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace FriendlyLogger.Logger
 {
@@ -14,7 +15,25 @@ namespace FriendlyLogger.Logger
 
         public override void Log(Type declaringType, Level level, object message, Exception exception)
         {
-            //
+            if (exception == null)
+            {
+                WriteFile(message);
+            }
+            else
+            {
+                string text = string.Format("Error: {0}", exception.HResult, message);
+                WriteFile(text);
+            }
+        }
+
+        private void WriteFile(object message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine(message);
+            }
         }
     }
 }
