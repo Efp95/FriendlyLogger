721cdce0ff7c109da7340e8f9ca7dc7c1f82938a
Efp95 <edsonflorespalma@gmail.com>
2016-05-18 03:42:50 -0500

    Add LevelMappingTest

---
+++ LoggerSolution/FriendlyLogger.Tests/Core/LevelMappingTest.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using FriendlyLogger.Core;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace FriendlyLogger.Tests.Core
+{
+    [TestFixture]
+    public class LevelMappingTest
+    {
+
+        [SetUp]
+        public void SetUp()
+        { }
+
+
+        [Test]
+        public void Should_ReturnOneItem_When_CollectionIsNull()
+        {
+            IEnumerable<Level> levels = null;
+
+            levels = LevelMapping.Execute(null);
+
+            levels.Should().NotBeNullOrEmpty().And.HaveCount(1);
+        }
+
+        [Test]
+        public void Should_ReturnDebugLevel_When_CollectionIsNull()
+        {
+            IEnumerable<Level> levels = null;
+
+            levels = LevelMapping.Execute(null);
+
+            using (var enumerator = levels.GetEnumerator())
+            {
+                if (enumerator.MoveNext())
+                    enumerator.Current.Name.Should().Be("DEBUG");
+            }
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        { }
+
+    }
+}
